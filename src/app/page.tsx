af91bede15a7a0af8c5dacadbbb1901f216af3e8
Fru Ngwa <dohng28@gmail.com>
2025-07-24 18:44:37 +0100

    Change Top bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,17 +107,17 @@ export default function Home() {
 
   return (
     <main>
+      <span>
+        <Link href="/">
+          <Image
+            src={est}
+            alt="Logo"
+            height={100}
+            style={{ cursor: "pointer" }}
+          />
+        </Link>
+      </span>
       <div className="top-bar">
-        <span>
-          <Link href="/">
-            <Image
-              src={est}
-              alt="Logo"
-              height={100}
-              style={{ cursor: "pointer" }}
-            />
-          </Link>
-        </span>
         <button className="watch-btn">
           <a href="/add-watch">+ Add Watch</a>
         </button>
